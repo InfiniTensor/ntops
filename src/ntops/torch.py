3da0418f1de79c7c1f9dc8e68ac354bf38d1ce64
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-07-25 14:50:05 +0800

    Add `layer_norm` operator

--- src/ntops/torch.py
+++ src/ntops/torch.py
@@ -23,6 +23,7 @@ import ntops.kernels.gelu
 import ntops.kernels.gt
 import ntops.kernels.isinf
 import ntops.kernels.isnan
+import ntops.kernels.layer_norm
 import ntops.kernels.le
 import ntops.kernels.lt
 import ntops.kernels.mm
@@ -252,6 +253,33 @@ def isnan(input):
     kernel = _cached_make(ntops.kernels.isnan.premake, input.ndim)
 
     kernel(input, output)
+
+    return output
+
+
+def layer_norm(input, normalized_shape, weight=None, bias=None, eps=1e-5):
+    if isinstance(normalized_shape, int):
+        normalized_shape = (normalized_shape,)
+
+    normalized_shape = tuple(normalized_shape)
+
+    if weight is None:
+        weight = torch.ones_like(input)
+    else:
+        weight = weight.expand_as(input)
+
+    if bias is None:
+        bias = torch.zeros_like(input)
+    else:
+        bias = bias.expand_as(input)
+
+    output = torch.empty_like(input)
+
+    kernel = _cached_make(
+        ntops.kernels.layer_norm.premake, input.ndim, normalized_shape
+    )
+
+    kernel(input, weight, bias, eps, output, math.prod(normalized_shape))
 
     return output
 
