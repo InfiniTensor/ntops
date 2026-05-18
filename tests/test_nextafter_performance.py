282416213c89ab434a62741ebd218f78cac65e84
HyosungSink <HyosungSink@users.noreply.github.com>
2026-05-18 12:42:38 +0000

    Register T1-1-1 ntops operators

---
+++ tests/test_nextafter_performance.py
@@ -0,0 +1,11 @@
+import pytest
+
+from tests.t1_1_1_performance_utils import perf_cases_for, run_perf_case
+
+
+_PERF_CASES = perf_cases_for("nextafter")
+
+
+@pytest.mark.parametrize("case", _PERF_CASES, ids=lambda case: case.case_name)
+def test_nextafter_performance(case):
+    run_perf_case(case)
