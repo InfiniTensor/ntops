41e019d118d529dad7efbb40b74705ab352b4e33
acoier <851563813@qq.com>
2026-06-16 17:01:36 +0800

    fix trapezoid ops implement
    Signed-off-by: acoier <851563813@qq.com>

--- src/ntops/kernels/trapezoid.py
+++ src/ntops/kernels/trapezoid.py
@@ -1,17 +1,29 @@
 import functools
 
+import ninetoothed
+import ninetoothed.language as ntl
 from ninetoothed import Tensor
 
-from ntops.kernels.element_wise import arrangement
+from ntops.kernels.reduction import arrangement
 
 
-def application(input, output):
-    output = input  # noqa: F841
+def application(areas, output):
+    dtype = output.dtype.dtype
+    total = ntl.cast(0, ntl.float32)
+
+    for i in range(areas.shape[0]):
+        total = total + ntl.cast(ntl.sum(areas[i]), ntl.float32)
+
+    for j in range(output.shape[0]):
+        output[j] = ntl.cast(total, dtype)
 
 
 def premake(ndim, dtype=None, block_size=None):
-    arrangement_ = functools.partial(arrangement, block_size=block_size)
+    arrangement_ = functools.partial(arrangement, dim=-1, block_size=block_size)
 
-    tensors = (Tensor(ndim, dtype=dtype), Tensor(ndim, dtype=dtype))
+    tensors = (
+        Tensor(ndim, dtype=dtype, other=0),
+        Tensor(ndim, dtype=dtype),
+    )
 
     return arrangement_, application, tensors
--- src/ntops/torch/trapezoid.py
+++ src/ntops/torch/trapezoid.py
@@ -5,27 +5,23 @@ from ntops.torch.utils import _cached_make
 
 
 def trapezoid(y, x=None, *, dx=1.0, dim=-1):
-    # Move the integration dimension to the last axis for uniform handling
     if dim != -1 and dim != y.ndim - 1:
         y = y.transpose(dim, -1)
         if x is not None and x.ndim == y.ndim:
             x = x.transpose(dim, -1)
 
     if x is None:
-        mid = (y[..., 1:] + y[..., :-1]) * 0.5
-        areas = mid * dx
+        areas = (y[..., 1:] + y[..., :-1]) * 0.5 * dx
     else:
         if x.ndim == 1:
             dx_vals = x[1:] - x[:-1]
         else:
             dx_vals = x[..., 1:] - x[..., :-1]
-        mid = (y[..., 1:] + y[..., :-1]) * 0.5
-        areas = mid * dx_vals
+        areas = (y[..., 1:] + y[..., :-1]) * 0.5 * dx_vals
 
-    output = torch.empty_like(areas)
+    output = torch.zeros(*areas.shape[:-1], 1, dtype=areas.dtype, device=areas.device)
+
     kernel = _cached_make(ntops.kernels.trapezoid.premake, areas.ndim)
     kernel(areas, output)
 
-    result = output.sum(dim=-1)
-
-    return result
+    return output.squeeze(-1)
